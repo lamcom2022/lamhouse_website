1a8cdcc8fa8989439072f6cf0a9d3a84a71977fb
R-Ravinther <61197918+R-Ravinther@users.noreply.github.com>
2022-09-02 21:37:08 +0530

    Update team.vue

--- pages/team.vue
+++ pages/team.vue
@@ -3,21 +3,21 @@
     <div class="max-w-7xl mx-auto mt-16 my-16 lg:grid lg:grid-cols-3 md:flex md:flex-col py-10">
         <div class="mx-auto px-2 lg:px-2 lg:py-10 md:py-10">
             <img class="w-full rounded" src="https://res.cloudinary.com/lamcom/image/upload/v1662126888/lamhouse/1-1_quo9fi.jpg">
-            <div class="box-border h-32 w-32 border-4 bg-primary-600 shadow-lg">
+            <div class="box-border h-32 w-full border-4 bg-primary-600 shadow-lg">
                 <p class="text-white text-center font-bold pt-10 text-2xl">Muthuraman</p>
                 <p class="text-white text-center font-normal text-xl">CEO, Founder</p>
             </div>
         </div>
         <div class="mx-auto px-2 lg:px-2 lg:py-10 md:py-10">
             <img class="w-full rounded-t" src="https://res.cloudinary.com/lamcom/image/upload/v1662126899/lamhouse/3-2_xsatub.jpg">
-            <div class="box-border h-32 w-32 border-4 bg-primary-600 shadow-lg">
+            <div class="box-border h-32 w-full border-4 bg-primary-600 shadow-lg">
                 <p class="text-white text-center font-bold pt-10 text-2xl">Muthuraman</p>
                 <p class="text-white text-center font-normal text-xl">CEO, Founder</p>
             </div>
         </div>
         <div class="mx-auto px-2 lg:px-2 lg:py-10 md:py-10">
             <img class="w-full rounded" src="https://res.cloudinary.com/lamcom/image/upload/v1662126895/lamhouse/qwqw_xpwur0.jpg">
-            <div class="box-border h-32 w-32 border-4 bg-primary-600 shadow-lg">
+            <div class="box-border h-32 w-full border-4 bg-primary-600 shadow-lg">
                 <p class="text-white text-center font-bold pt-10 text-2xl">Muthuraman</p>
                 <p class="text-white text-center font-normal text-xl">CEO, Founder</p>
             </div>
