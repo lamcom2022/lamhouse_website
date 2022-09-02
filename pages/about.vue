c77880e76f8ee995a507c990cb489756f233a4a1
R-Ravinther <61197918+R-Ravinther@users.noreply.github.com>
2022-09-02 21:21:04 +0530

    updated pages

--- pages/about.vue
+++ pages/about.vue
@@ -36,7 +36,7 @@
 			</NuxtLink>-->
 		</div>
 	</main>
-  <div class="grid grid-cols-2 gap-4 mt-16 my-20 max-w-7xl mx-auto">
+  <div class="max-w-7xl mx-auto mt-16 my-16 lg:grid lg:grid-cols-2 md:flex md:flex-col">
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
      <img src="https://res.cloudinary.com/lamcom/image/upload/v1662041293/lamhouse/about-people-768x845_xqwaiz.jpg">
      <div class="box-content bg-primary-600 mt-2 w-full h-32 rounded-lg">
@@ -44,12 +44,12 @@
      </div>
       </div>
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-      <div class="lg:text-center">
+      <div class="lg:text-center md:text-center">
         <h2 class="text-base text-indigo-600 font-semibold tracking-wide uppercase">About Us</h2>
         <p class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
          Welcome to Lamhouse
         </p>
-        <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">
+        <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto md:mx-auto">
           LAM House symbolises Brilliance, Reliability and Integrity
         </p>
       </div>
@@ -62,21 +62,43 @@
                 <component :is="about.icon" class="h-6 w-6" aria-hidden="true" />
               </div>
               <p class="ml-16 text-lg leading-6 font-medium text-gray-900">{{ about.name }}</p>
-            </dt>
-            <dd class="mt-2 ml-16 text-base text-gray-500">
-              {{ about.description }}
-            </dd>
+            </dt>            
           </div>
         </dl>
-      </div>
+      </div>      
     </div>
-
   </div>
+
+  <div class="max-w-7xl mx-auto mt-16 my-16 lg:grid">
+     <div class="max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div class="lg:text-left md:text-left">
+        <h2 class="text-base text-indigo-600 font-semibold tracking-wide uppercase">About Us</h2>
+        <p class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+         The Name and Legacy
+        </p>        
+      </div>
+   </div>
+     <div class="mt-10 max-w-7xl">
+        <dl class="space-y-10 md:space-y-0 md:grid md:gap-y-10">
+          <div v-for="legacy in legacy" :key="legacy.name" class="relative">
+            <dt>
+              <div class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                <component :is="legacy.icon" class="h-6 w-6" aria-hidden="true" />
+              </div>
+              <p class="ml-16 text-lg leading-6 font-medium text-gray-900">{{ legacy.name }}</p>
+            </dt>            
+          </div>
+        </dl>
+      </div> 
+   </div>
+
+   
+  
   
 </template>
 
 <script>
-import { AnnotationIcon, GlobeAltIcon, LightningBoltIcon, ScaleIcon, ArrowDownIcon } from '@heroicons/vue/outline/index.js'
+import { AnnotationIcon, GlobeAltIcon, LightningBoltIcon, ScaleIcon, ArrowCircleRightIcon } from '@heroicons/vue/outline/index.js'
 definePageMeta({
   //colorMode: 'system',
   layout: "main",
@@ -84,31 +106,43 @@ definePageMeta({
 const about = [
   {
     name: 'Our Goal is: Every business idea must grow and have an online presence – however big or small!!',    
-    icon: GlobeAltIcon,
+    icon: ArrowCircleRightIcon,
   },
   {
     name: 'Today technology rules the world! We began our journey, in the year 2012, in the domain of 2D and 3D Animation.',    
-    icon: ScaleIcon,
+    icon: ArrowCircleRightIcon,
   },
   {
     name: 'Success followed us after our debut. Yet, in keeping with changing trends, we quickly expanded our services, to include graphic design, website design and app development in our repertoire of projects.',    
-    icon: LightningBoltIcon,
+    icon: ArrowCircleRightIcon,
   },
   {
     name: 'It has been 10 years, and the journey has been incredible.',   
-    icon: AnnotationIcon,
+    icon: ArrowCircleRightIcon,
   },
   {
     name: 'With every year, our expertise and reputed clientele list grew bigger and bigger, both nationally and internationally. Today we positively state that we have numerous happy and satisfied clients whose visions and ideas we have brought to life.',   
-    icon: AnnotationIcon,
+    icon: ArrowCircleRightIcon,
   },
 ]
 
+const legacy = [
+  {
+    name: 'The name LAM House is inspired by and dedicated to my parents and family. LAM is an acronym for Life, Art and Multimedia.',    
+    icon: ArrowCircleRightIcon,
+  },
+  {
+    name: 'Mr. Muthuraman A. as the proprietor of this company brings along with him over 20 years of experience in the fields of animation and graphic design. From managerial roles, to HR roles to administrative details to the financial aspects of a graphic designing and software development firm. In the past years he has grown and groomed our team to reflect these qualities of integrity and dedication in their work.',    
+    icon: ArrowCircleRightIcon,
+  },  
+]
 export default {
   setup() {
     return {
       about,
-    }
+      legacy,     
+    }   
   },
+ 
 }
 </script>
