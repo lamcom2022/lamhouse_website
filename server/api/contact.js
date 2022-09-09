7d7d9ded5035a299df5a9176bde2e80bd7678b49
Pradeep HGK <apple@Apples-MacBook-Pro.local>
2022-09-09 17:50:10 +0530

    Added Send grid config details

--- server/api/contact.js
+++ server/api/contact.js
@@ -1,3 +1,5 @@
+const sg = require('../sendemail');
+
 export default async (req, res) => {
     const body = await useBody(req)
     //console.log(body)
@@ -11,5 +13,21 @@ export default async (req, res) => {
             "document": body
         }
     })
+
+
+    const msg = {
+        to: body.email, // Change to your recipient
+        from: 'service@lamhouse.com', // Change to your verified sender
+        subject: 'lamhouse',
+        text: 'and easy to do anywhere, even with Node.js',
+        html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+    }
+
+    sg.send(msg).then(() => {
+        console.log('Email sent')
+    }).catch((error) => {
+        console.error(error)
+    })
+
     return contact
 }
