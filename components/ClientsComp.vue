6d9a8b160dde8c0aad7acea31e0a0083a72ef383
R-Ravinther <61197918+R-Ravinther@users.noreply.github.com>
2022-09-10 13:24:29 +0530

    updated clients

--- components/ClientsComp.vue
+++ components/ClientsComp.vue
@@ -12,6 +12,9 @@
         <img class="w-full aspect-auto object-none object-center" src="https://res.cloudinary.com/lamcom/image/upload/v1662791566/lamhouse/clientlogo/advik_t23t7y.png">
         </div>
         <div class="flex justify-center w-full bg-white shadow-lg h-64">
+        <img class="w-full aspect-auto object-none object-center" src="https://res.cloudinary.com/lamcom/image/upload/v1662795985/lamhouse/clientlogo/bigbee_uztmzk.png">
+        </div>
+        <div class="flex justify-center w-full bg-white shadow-lg h-64">
         <img class="w-full aspect-auto object-none object-center" src="https://res.cloudinary.com/lamcom/image/upload/v1662789364/lamhouse/clientlogo/bigbull_ge3h5x.png">
         </div>
         <div class="flex justify-center w-full bg-white shadow-lg h-64">
@@ -25,6 +28,9 @@
         </div>
         <div class="flex justify-center w-full bg-white shadow-lg h-64">
         <img class="w-full aspect-auto object-none object-center" src="https://res.cloudinary.com/lamcom/image/upload/v1662727824/lamhouse/clientlogo/dhi_f3y1zs.png">
+        </div>
+        <div class="flex justify-center w-full bg-white shadow-lg h-64">
+        <img class="w-full aspect-auto object-none object-center" src="https://res.cloudinary.com/lamcom/image/upload/v1662795146/lamhouse/clientlogo/dredgepro_io3lfe.png">
         </div>
         <div class="flex justify-center w-full bg-white shadow-lg h-64">
         <img class="w-full aspect-auto object-none object-center" src="https://res.cloudinary.com/lamcom/image/upload/v1662728232/lamhouse/clientlogo/fitbe_sig6sm.png">
@@ -51,6 +57,9 @@
         <img class="w-full aspect-auto object-none object-center" src="https://res.cloudinary.com/lamcom/image/upload/v1662791309/lamhouse/clientlogo/mkagencies_uezpq1.png">
         </div>
         <div class="flex justify-center w-full bg-white shadow-lg h-64">
+        <img class="w-full aspect-auto object-none object-center" src="https://res.cloudinary.com/lamcom/image/upload/v1662795565/lamhouse/clientlogo/massavi_tfefmx.png">
+        </div>
+        <div class="flex justify-center w-full bg-white shadow-lg h-64">
         <img class="w-full aspect-auto object-none object-center" src="https://res.cloudinary.com/lamcom/image/upload/v1662792644/lamhouse/clientlogo/navipower_i9rsc1.png">
         </div>
         <div class="flex justify-center w-full bg-white shadow-lg h-64">
@@ -67,6 +76,18 @@
         </div>
         <div class="flex justify-center w-full bg-white shadow-lg h-64">
         <img class="w-full aspect-auto object-none object-center" src="https://res.cloudinary.com/lamcom/image/upload/v1662793981/lamhouse/clientlogo/sakthi_bfc6sb.png">
+        </div>
+        <div class="flex justify-center w-full bg-white shadow-lg h-64">
+        <img class="w-full aspect-auto object-none object-center" src="https://res.cloudinary.com/lamcom/image/upload/v1662794931/lamhouse/clientlogo/tavara_lbif1j.png">
+        </div>
+        <div class="flex justify-center w-full bg-white shadow-lg h-64">
+        <img class="w-full aspect-auto object-none object-center" src="https://res.cloudinary.com/lamcom/image/upload/v1662795030/lamhouse/clientlogo/techdeal_gl9prv.png">
+        </div>
+        <div class="flex justify-center w-full bg-white shadow-lg h-64">
+        <img class="w-full aspect-auto object-none object-center" src="https://res.cloudinary.com/lamcom/image/upload/v1662795819/lamhouse/clientlogo/veera_chpvm4.png">
+        </div>
+        <div class="flex justify-center w-full bg-white shadow-lg h-64">
+        <img class="w-full aspect-auto object-none object-center" src="https://res.cloudinary.com/lamcom/image/upload/v1662796236/lamhouse/clientlogo/zynomi_ctva4e.png">
         </div>
         
         
