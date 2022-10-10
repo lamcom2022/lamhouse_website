aee5c2f592ade6a08d65eb8195794d02e1c29c51
Pradeep HGK <pradeephari1594@gmail.com>
2022-10-10 23:03:16 +0530

    Added images and icons

--- components/FooterLinks.vue
+++ components/FooterLinks.vue
@@ -32,9 +32,6 @@
             </div>
          </div>
          <div class="grid grid-cols-2">
-           
-       
-                         
             <div class="mt-12 md:mt-0 md:min-w-64 lg:w-full">
                 <iframe class="md:min-w-64" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3889.489847774526!2d77.6548749151321!3d12.876192590918535!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae6c9d9255fc4d%3A0x6a652194251fe657!2sLAM%20House!5e0!3m2!1sen!2sin!4v1662564565090!5m2!1sen!2sin" width="333" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
