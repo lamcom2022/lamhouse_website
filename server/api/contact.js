3b661c4846447744b13212060f932713ded82f94
Pradeep HGK <pradeephari1594@gmail.com>
2022-10-11 09:31:03 +0530

    Exposed db details from env

--- server/api/contact.js
+++ server/api/contact.js
@@ -1,12 +1,12 @@
 export default async (req, res) => {
     const body = await useBody(req)
-    const contact = await $fetch(`${process.env.MONGODB_ATLAS_REST_URL}/insertOne`, {
+    const contact = await $fetch(`https://data.mongodb-api.com/app/data-fgorq/endpoint/data/v1/action/insertOne`, {
         method: 'post', headers: {
-            "api-key": `${process.env.MONGODB_ATLAS_REST_TOKEN}`
+            "api-key": `KTzPhWk0W9fQHmSuQFxK2enn8wjSJ71y1pz3MdaYrzQ2RNcFT4N0mFfaNEweKR2k`
         }, body: {
             "collection": "contacts",
-            "database": `${process.env.MONGODB_ATLAS_DATABASE}`,
-            "dataSource": `${process.env.MONGODB_ATLAS_DATASOURCE}`,
+            "database": `lamhouse-webapp-db`,
+            "dataSource": `Cluster0`,
             "document": body
         }
     })
