4979b2448dcf02601cad1ea792adfd577b8217f2
R-Ravinther <61197918+R-Ravinther@users.noreply.github.com>
2022-10-12 12:44:20 +0530

    created a form

---
+++ pages/form/index.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+    
+     <Form />
+    
+  </div>
+
+</template>
+
+<script setup>
+import ClientsComp from '@/components/ClientsComp.vue'
+definePageMeta({
+  //colorMode: 'system',
+  layout: "main",
+})
+</script>
