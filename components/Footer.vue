96eaa2dffe7f31065b043e088e0d5ef637969123
R-Ravinther <61197918+R-Ravinther@users.noreply.github.com>
2022-10-26 20:08:38 +0530

    Update Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -141,7 +141,7 @@
             >Contact</a
           >
         </nav>
-        <nav class="col-span-3 md:col-span-1 lg:col-span-3">
+        <nav class="col-span-3 md:col-span-1 lg:col-span-3 ">
           <p
             class="
               mb-3
@@ -178,6 +178,7 @@
               transition
               md:mb-2
               hover:text-white
+              
             "
           >
             Above union bank, 2nd floor
@@ -192,6 +193,7 @@
               transition
               md:mb-2
               hover:text-white
+             
             "
           >
             Basavanagudi
@@ -206,6 +208,7 @@
               transition
               md:mb-2
               hover:text-white
+             
             "
           >
             Bangalore 560004
@@ -265,36 +268,39 @@
             </svg>
             9886357328, 9880303209
           </p>
-          <p
+          
+        
+       <p
             class="
               flex
+              mb-3 
+              text-blue-100
+              transition
+              md:mb-2
+              hover:text-white
+              gap-5 
+                    
+            "
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-fill flex-1" viewBox="0 0 16 16">
+  <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757Zm3.436-.586L16 11.801V4.697l-5.803 3.546Z"/>
+</svg>
+       </p>  
+ <p
+            class="
+              flex 
               mb-3
               text-sm
               font-medium
               text-blue-100
               transition
               md:mb-2
-              hover:text-white
-              gap-5             
+              hover:text-white                         
             "
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              class="bi bi-envelope-check-fill"
-              viewBox="0 0 16 16"
-            >
-              <path
-                d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555ZM0 4.697v7.104l5.803-3.558L0 4.697ZM6.761 8.83l-6.57 4.026A2 2 0 0 0 2 14h6.256A4.493 4.493 0 0 1 8 12.5a4.49 4.49 0 0 1 1.606-3.446l-.367-.225L8 9.586l-1.239-.757ZM16 4.697v4.974A4.491 4.491 0 0 0 12.5 8a4.49 4.49 0 0 0-1.965.45l-.338-.207L16 4.697Z"
-              />
-              <path
-                d="M16 12.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Zm-1.993-1.679a.5.5 0 0 0-.686.172l-1.17 1.95-.547-.547a.5.5 0 0 0-.708.708l.774.773a.75.75 0 0 0 1.174-.144l1.335-2.226a.5.5 0 0 0-.172-.686Z"
-              />
-            </svg>
             services.beyondbordersindia.in
           </p>
+         
         </nav>
       </div>
       <div
