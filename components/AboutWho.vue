a5a92327e2a3b565a0f2925f62d44f117ddcf8e6
R-Ravinther <61197918+R-Ravinther@users.noreply.github.com>
2022-11-02 20:03:17 +0530

    Update AboutWho.vue

--- components/AboutWho.vue
+++ components/AboutWho.vue
@@ -1,5 +1,5 @@
 <template>
- <div class="bg-white pt-5 pb-5 px-4 sm:px-6 lg:pt-12 lg:pb-12 lg:px-8">
+  <div class="bg-white pt-5 pb-5 px-4 sm:px-6 lg:pt-12 lg:pb-12 lg:px-8">
     <div
       class="
         grid
@@ -19,23 +19,43 @@
             font-normal
             text-primary-500
             sm:text-2xl
-            
           "
         ></h3>
         <h2
-          class="text-xl text-center tracking-tight font-bold text-primary-900 sm:text-2xl"
+          class="
+            text-xl text-center
+            tracking-tight
+            font-bold
+            text-primary-900
+            sm:text-2xl
+          "
         >
           About Beyond Borders
         </h2>
         <p class="mt-6 mb-6 text-base font-normal text-center">
-          Beyond Borders is managed by <span class="uppercase text-primary-900 font-bold">Mrs. Swathi Gowda</span>, who is a skilled real
-          estate agent with excellent skills in property management. She has the
-          expertise in this field for 5+ years and there has been no looking
-          back since then. If you are looking for a trustworthy, honest and
-          effecient support, you have landed at the right place. Click here and
-          reach out to us.
+          Beyond Borders is managed by
+          <span class="uppercase text-primary-900 font-bold"
+            >Mrs. Swathi Gowda</span
+          >, a skilled real estate agent with excellency in property management.
+          Swathi has expertise in this field for 5+ years and there has been no
+          looking back with beyond borders. If you are looking for a
+          trustworthy, honest and effecient partner, you have landed at the
+          right place. Beyond borders will ensure all your property and home
+          needs are met with the necessary attention from our professional
+          teams. <br />Click here and reach out to us!
+          <a class="text-primary-900" href="https://beyondbordersindia.in/"
+            >https://beyondbordersindia.in/</a
+          >
         </p>
-       
+        <p class="mt-6 mb-6 text-base font-normal text-center">
+          Please reach out to us if your interested in job opportunities or
+          would like to join our staff. Being apart of Beyond borders’
+          commitment will provide the knowledge to effectively increase your
+          property management skills. We also encourage outside work and labor
+          forces to join our commitments along side our contractors. Beyond
+          borders is continually building a network of success with a team of
+          professionals always working for your property needs.
+        </p>
       </div>
 
       <!-- <div class="mx-auto w-auto relative mt-12 mr-0">
@@ -46,7 +66,17 @@
     </div>
   </div>
   <!-- who we are -->
-  <section class="relative block py-24 overflow-hidden leading-6 text-left bg-primary-900">
+  <section
+    class="
+      relative
+      block
+      py-24
+      overflow-hidden
+      leading-6
+      text-left
+      bg-primary-900
+    "
+  >
     <div class="w-full max-w-5xl px-8 mx-auto leading-6 text-left xl:px-12">
       <div
         class="
@@ -59,9 +89,7 @@
           lg:flex-grow-0 lg:flex-shrink-0
         "
       >
-        <h2
-          class="text-xl tracking-tight font-bold text-white sm:text-2xl"
-        >
+        <h2 class="text-xl tracking-tight font-bold text-white sm:text-2xl">
           Who We Are
         </h2>
         <p
@@ -87,55 +115,213 @@
     </div>
   </section>
 
-   <!-- Why us -->
-    <section class="bg-white max-w-7xl pt-5 pb-5 px-4 lg:pt-12 lg:pb-12 mx-auto">
-        <div>
-            <div class="flex py-12 sm:py-12 xl:py-16 lg:flex-row flex-col justify-center items-center">
-                <div class="w-full lg:w-1/2 lg:px-0 sm:px-7 lg:pl-0 xl:pl-10 mb-16 lg:mb-0">
-                    <div class="relative">
-                        <h2 class="mb-5 xl:mb-12 text-xl sm:text-2xl leading-tight md:leading-tight lg:leading-tight text-primary-900 font-bold">Why Us</h2>
-                        <div class="flex mb-6 xl:pr-0 lg:pr-12">
-                            <svg class="w-8 h-8 -mt-2 flex-shrink-0 mr-3 xl:mr-5 text-primary-900" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
-                            <div class="max-w-md lg:max-w-xl">
-                                <h3 class="mb-2 -mt-1 text-xl md:text-xl font-semibold text-gray-700">Experienced Staff</h3>
-                                <p class="text-gray-900 leading-loose">You can always be assured of exceptional service with our expert insights and experienced staff.</p>
-                            </div>
-                        </div>
-                        <div class="flex mb-6 xl:pr-0 lg:pr-12">
-                            <svg class="w-8 h-8 -mt-2 flex-shrink-0 mr-3 xl:mr-5 text-primary-900" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
-                            <div class="max-w-md lg:max-w-xl">
-                                <h3 class="mb-2 -mt-1 text-xl md:text-xl font-semibold text-gray-700">Dedicated Team</h3>
-                                <p class="text-gray-900 leading-loose">Our dedicated team ensures that your valuable asset is managed to the highest standard.</p>
-                            </div>
-                        </div>
-                        <div class="flex mb-6 xl:pr-0 lg:pr-12">
-                            <svg class="w-8 h-8 -mt-2 flex-shrink-0 mr-3 xl:mr-5 text-primary-900" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
-                            <div class="max-w-md lg:max-w-xl">
-                                <h3 class="mb-2 -mt-1 text-xl md:text-xl font-semibold text-gray-700">Professional Approach</h3>
-                                <p class="text-gray-900 leading-loose">Our hands-on approach focuses closely on the day-to-day activities of property management. Our professional approach further alleviates your concerns by providing an expert service incorporating direct communication and regular reporting</p>
-                            </div>
-                        </div>
-                         <div class="flex mb-6 xl:pr-0 lg:pr-12">
-                            <svg class="w-8 h-8 -mt-2 flex-shrink-0 mr-3 xl:mr-5 text-primary-900" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
-                            <div class="max-w-md lg:max-w-xl">
-                                <h3 class="mb-2 -mt-1 text-xl md:text-xl font-semibold text-gray-700">Trusted Partners</h3>
-                                <p class="text-gray-900 leading-loose">Over 10 expatriates have trusted over 15 years to manage their properties and have become happy customers.</p>
-                            </div>
-                        </div>
-
-                    </div>
-                </div>
-                <div class="mx-auto w-auto relative mt-12 mr-0">
-                    
-                        <img src="https://res.cloudinary.com/lamcom/image/upload/v1666965756/beyondborder/about/whyus-small_d8kbwm.jpg">
-                    
-                </div>
+  <!-- Why us -->
+  <section class="bg-white max-w-7xl pt-5 pb-5 px-4 lg:pt-12 lg:pb-12 mx-auto">
+    <div>
+      <div
+        class="
+          flex
+          py-12
+          sm:py-12
+          xl:py-16
+          lg:flex-row
+          flex-col
+          justify-center
+          items-center
+        "
+      >
+        <div
+          class="w-full lg:w-1/2 lg:px-0 sm:px-7 lg:pl-0 xl:pl-10 mb-16 lg:mb-0"
+        >
+          <div class="relative">
+            <h2
+              class="
+                mb-5
+                xl:mb-12
+                text-xl
+                sm:text-2xl
+                leading-tight
+                md:leading-tight
+                lg:leading-tight
+                text-primary-900
+                font-bold
+              "
+            >
+              Why Us
+            </h2>
+            <div class="flex mb-6 xl:pr-0 lg:pr-12">
+              <svg
+                class="
+                  w-8
+                  h-8
+                  -mt-2
+                  flex-shrink-0
+                  mr-3
+                  xl:mr-5
+                  text-primary-900
+                "
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+              <div class="max-w-md lg:max-w-xl">
+                <h3
+                  class="
+                    mb-2
+                    -mt-1
+                    text-xl
+                    md:text-xl
+                    font-semibold
+                    text-gray-700
+                  "
+                >
+                  Experienced Staff
+                </h3>
+                <p class="text-gray-900 leading-loose">
+                  You can always be assured of exceptional service with our
+                  expert insights and experienced staff.
+                </p>
+              </div>
             </div>
-           
+            <div class="flex mb-6 xl:pr-0 lg:pr-12">
+              <svg
+                class="
+                  w-8
+                  h-8
+                  -mt-2
+                  flex-shrink-0
+                  mr-3
+                  xl:mr-5
+                  text-primary-900
+                "
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+              <div class="max-w-md lg:max-w-xl">
+                <h3
+                  class="
+                    mb-2
+                    -mt-1
+                    text-xl
+                    md:text-xl
+                    font-semibold
+                    text-gray-700
+                  "
+                >
+                  Dedicated Team
+                </h3>
+                <p class="text-gray-900 leading-loose">
+                  Our dedicated team ensures that your valuable asset is managed
+                  to the highest standard.
+                </p>
+              </div>
+            </div>
+            <div class="flex mb-6 xl:pr-0 lg:pr-12">
+              <svg
+                class="
+                  w-8
+                  h-8
+                  -mt-2
+                  flex-shrink-0
+                  mr-3
+                  xl:mr-5
+                  text-primary-900
+                "
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+              <div class="max-w-md lg:max-w-xl">
+                <h3
+                  class="
+                    mb-2
+                    -mt-1
+                    text-xl
+                    md:text-xl
+                    font-semibold
+                    text-gray-700
+                  "
+                >
+                  Professional Approach
+                </h3>
+                <p class="text-gray-900 leading-loose">
+                  Our hands-on approach focuses closely on the day-to-day
+                  activities of property management. Our professional approach
+                  further alleviates your concerns by providing an expert
+                  service incorporating direct communication and regular
+                  reporting
+                </p>
+              </div>
+            </div>
+            <div class="flex mb-6 xl:pr-0 lg:pr-12">
+              <svg
+                class="
+                  w-8
+                  h-8
+                  -mt-2
+                  flex-shrink-0
+                  mr-3
+                  xl:mr-5
+                  text-primary-900
+                "
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+              <div class="max-w-md lg:max-w-xl">
+                <h3
+                  class="
+                    mb-2
+                    -mt-1
+                    text-xl
+                    md:text-xl
+                    font-semibold
+                    text-gray-700
+                  "
+                >
+                  Trusted Partners
+                </h3>
+                <p class="text-gray-900 leading-loose">
+                  Over 10 expatriates have trusted over 15 years to manage their
+                  properties and have become happy customers.
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
-    </section>
-
-
+        <div class="mx-auto w-auto relative mt-12 mr-0">
+          <img
+            src="https://res.cloudinary.com/lamcom/image/upload/v1666965756/beyondborder/about/whyus-small_d8kbwm.jpg"
+          />
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
