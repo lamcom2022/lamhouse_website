ea25c8ce8d2b4e6e4f67f6be15decddaddc06674
R-Ravinther <61197918+R-Ravinther@users.noreply.github.com>
2022-11-18 11:27:11 +0530

    updated index-1

---
+++ layouts/home.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="min-h-screen relative">
+        <div class="h-full relative">
+            <Head />
+            <slot />
+            <Foot />
+            <ContactSales />
+            <SignUp />
+        </div>
+    </div>
+</template>
---
+++ pages/index-1.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <HomeHero />
+    <HomeBody :articles="articles" />    
+   
+  </div>
+
+</template>
+
+<script setup>
+definePageMeta({
+  //colorMode: 'system',
+  layout: "home",
+})
+const { data: color } = ref('white')
+const { data: articles } = await useAsyncData('articles-list', () => queryContent('articles')
+  .only([
+    "title",
+    "description",
+    "link",
+    "coverimage",
+    "author",
+    "date",
+    "_path",
+    "tags",
+    "type",
+  ])
+  .find())
+</script>
