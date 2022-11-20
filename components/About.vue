a65eecad9a7ede0997fa90152f2bb683d68905c1
R-Ravinther <61197918+R-Ravinther@users.noreply.github.com>
2022-11-20 22:07:54 +0530

    updated about

---
+++ components/About.vue
@@ -0,0 +1,140 @@
+<template>
+<main
+		class="mx-auto bg-gradient-to-r from-primary-800 via-primary-900 to-primary-800 ... lg:max-w-8xl px-5 lg:px-0 pt-14 lg:pt-12">
+		<h1 class="capitalize font-semibold text-[38px] lg:text-7xl text-center text-white h-64 py-16">			
+			
+			<span class="font-extrabold bg-clip-text text-primary-500  uppercase">About</span>
+		</h1>
+	</main>
+ <!-- Section 2 -->
+    <section class="w-full bg-white md:pt-12 md:pb-24">
+        <div class="box-border flex flex-col items-center content-center px-8 mx-auto leading-6 text-black border-0 border-gray-300 border-solid md:flex-row max-w-7xl lg:px-16">
+
+            <!-- Image -->
+            <div class="box-border relative w-full max-w-md px-4 mt-5 mb-4 -ml-5 text-center rounded-md md:ml-0 md:mt-0 md:max-w-none lg:mb-0 md:w-1/2 xl:pl-10">
+                <img src="https://res.cloudinary.com/lamcom/image/upload/v1668697817/sravya/images/prof_m3kjtq.jpg">
+            </div>
+
+            <!-- Content -->
+            <div class="box-border order-first w-full text-black border-solid md:w-1/2 md:pl-10 md:order-none">
+                <h2 class="m-0 text-xl font-semibold leading-tight border-0 border-gray-300 lg:text-3xl md:text-2xl">
+                    Introduction
+                </h2>
+                <p class="pt-4 pb-4 m-0 leading-7 border-0 border-gray-300 sm:pr-12 xl:pr-32 lg:text-lg">
+                    SRAVYA FOOD AND BEVERAGES, established in May 2018, is a Proprietorship Company. 
+                </p>
+                <p class="pt-4 pb-4 leading-7">The Founder/CEO, Mr. Chakrapani has gathered vast experience in managing and marketing companies along with creating innovative solutions for these companies. </p>
+                <p>Sravya Food and Beverages deals with Food and Agro-based commodities across the globe.</p>
+               
+                
+            </div>
+            <!-- End  Content -->
+        </div>       
+    </section>
+
+    <!-- Section 17 -->
+      <section class="pt-12 pb-8 leading-7 text-white bg-primary-900">
+          <div class="relative px-12 mx-auto w-full max-w-7xl text-white box-border lg:pt-12 md:pt-24 lg:pb-12 md:pb-12">
+              <div class="flex flex-wrap -mx-5 mt-0 box-border">
+                  <!-- Feature 1 -->
+                  <div class="relative flex-none px-5 mt-0 w-full max-w-full md:w-1/3 md:flex-none box-border">
+                      <div class="mb-3 w-12 h-12 rounded-md bg-gradient-to-r from-primary-500 to-primary-700 inline-block align-middle py-2">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="white" class="bi bi-eye mx-auto items-baseline" viewBox="0 0 16 16">
+                          <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
+                          <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z"/> 
+                          </svg>
+                      </div>
+                      <h3 class="mt-0 mb-2 font-extrabold text-xl tracking-normal box-border"> Vision Statement  </h3>
+                      <p class="mt-0 mb-8 md:mb-0 box-border text-gray-200"> To be a Globally Recognized Import- Export Company, that prioritizes service, quality, and satisfaction of its customers, employees and stakeholders. </p>
+                  </div>
+                  <!-- Feature 2 -->
+                  <div class="relative flex-none px-5 mt-0 w-full max-w-full md:w-1/3 md:flex-none box-border">
+                     <div class="mb-3 w-12 h-12 rounded-md bg-gradient-to-r from-primary-500 to-primary-700 inline-block align-middle py-2">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="white" class="bi bi-eye mx-auto items-baseline" viewBox="0 0 512 512">
+                          <path d="M448 256c0-106-86-192-192-192S64 150 64 256s86 192 192 192s192-86 192-192zm64 0c0 141.4-114.6 256-256 256S0 397.4 0 256S114.6 0 256 0S512 114.6 512 256zM256 336c44.2 0 80-35.8 80-80s-35.8-80-80-80s-80 35.8-80 80s35.8 80 80 80zm0 64c-79.5 0-144-64.5-144-144s64.5-144 144-144s144 64.5 144 144s-64.5 144-144 144zm32-144c0 17.7-14.3 32-32 32s-32-14.3-32-32s14.3-32 32-32s32 14.3 32 32z"/>
+                         
+                          </svg>
+                      </div>
+                     <h3 class="mt-0 mb-2 font-extrabold text-xl tracking-normal box-border"> Mission Statement  </h3>
+                      <p class="mt-0 mb-8 md:mb-0 box-border text-gray-200"> Sravya Food And Beverages aspires to offer customers a seamless experience with products of superior quality, across the globe, thereby contributing to the growth of society along with personal growth of business.  </p>
+                  </div>
+
+                  <!-- Feature 1 -->
+                  <div class="relative flex-none px-5 mt-0 w-full max-w-full md:w-1/3 md:flex-none box-border">
+                      <div class="mb-3 w-12 h-12 rounded-md bg-gradient-to-r from-primary-500 to-primary-700 inline-block align-middle py-2">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="white" class="bi bi-eye mx-auto items-baseline" viewBox="0 0 107.6 122.88">
+                          <path d="M53.8,0c7.76,0,14.55,7.23,19.26,18.92.56,1.4,1.1,2.88,1.61,4.41q2.35-.6,4.59-1c12.37-2.33,22.12-.49,26.3,6.05s1.75,16.15-5.56,26.4a81.46,81.46,0,0,1-5.48,6.82A80.84,80.84,0,0,1,100,68.44c7.24,10.3,9.6,19.93,5.38,26.45s-14,8.27-26.34,5.86c-1.43-.28-2.89-.62-4.39-1-.49,1.46-1,2.87-1.54,4.21-4.71,11.69-11.5,18.92-19.26,18.92S39.25,115.65,34.54,104c-.56-1.4-1.1-2.88-1.61-4.41q-2.36.6-4.59,1c-12.37,2.33-22.12.49-26.3-6S.29,78.37,7.6,68.12a79.48,79.48,0,0,1,5.48-6.82,80.84,80.84,0,0,1-5.43-6.86C.41,44.14-2,34.51,2.27,28s14-8.27,26.34-5.86c1.43.28,2.89.62,4.39,1,.49-1.46,1-2.87,1.54-4.21C39.25,7.23,46,0,53.8,0Zm-.14,55.8c1.8-1.87,3.06-3.49,5.83-3.81,5.21-.6,10,4.73,7.36,10-1.7,3.4-6.5,7.47-9.28,10.23l-3.91,3.87L50.44,73C46.55,69.21,40.22,64.5,40,58.67,39.86,54.58,43.09,52,46.8,52c3.31.05,4.7,1.69,6.86,3.79Zm37.07,9.79A123.59,123.59,0,0,1,79.51,75.5a120.11,120.11,0,0,1-3.33,19c1.31.33,2.6.63,3.86.87,10.11,2,17.81,1,20.69-3.45s.65-11.87-5.27-20.3a72.83,72.83,0,0,0-4.73-6Zm-73.86-8.3a123.59,123.59,0,0,1,11.22-9.91,120.11,120.11,0,0,1,3.33-19c-1.31-.33-2.6-.63-3.86-.87-10.11-2-17.81-1-20.69,3.45s-.65,11.87,5.27,20.3a72.83,72.83,0,0,0,4.73,6ZM69.33,98.2a116.42,116.42,0,0,1-15.64-6.48A116.73,116.73,0,0,1,38.21,98c.45,1.36.93,2.66,1.43,3.9,3.84,9.55,8.87,15.46,14.16,15.46S64.12,111.47,68,101.92c.48-1.19.94-2.43,1.37-3.72Zm-21.7-9.69c-2.72-1.53-5.46-3.18-8.19-5h0c-1.81-1.17-3.58-2.37-5.29-3.59a107.28,107.28,0,0,0,2.51,12.81,106.23,106.23,0,0,0,11-4.26ZM28.08,75.4A125,125,0,0,1,16.84,65.33a73.26,73.26,0,0,0-4.77,6c-6,8.4-8.26,15.82-5.41,20.28s10.54,5.5,20.67,3.59c1.31-.24,2.66-.54,4-.89A119.33,119.33,0,0,1,28.08,75.4ZM38.27,24.68a117,117,0,0,1,15.64,6.48,116.73,116.73,0,0,1,15.48-6.3C68.94,23.5,68.46,22.2,68,21,64.12,11.41,59.09,5.5,53.8,5.5S43.48,11.41,39.64,21c-.48,1.19-.94,2.43-1.37,3.72ZM60,34.37c2.72,1.53,5.46,3.18,8.19,5q2.72,1.77,5.3,3.6A109.3,109.3,0,0,0,71,30.11a106.23,106.23,0,0,0-11,4.26ZM79.52,47.48A125,125,0,0,1,90.76,57.55a73.26,73.26,0,0,0,4.77-6c6-8.4,8.26-15.82,5.41-20.28S90.4,25.8,80.27,27.71c-1.31.24-2.66.54-4,.89a119.33,119.33,0,0,1,3.29,18.88ZM87,61.56c-2.13-2.18-4.46-4.37-7-6.53.1,2.11.14,4.25.14,6.41S80.13,65.84,80,68c2.5-2.13,4.83-4.29,7-6.44ZM74.25,50.32Q70,47,65.17,43.92h0c-3.77-2.45-7.56-4.66-11.29-6.61-3.71,1.91-7.47,4.07-11.23,6.46l-.17.11c-3.2,2-6.25,4.18-9.12,6.35-.29,3.62-.44,7.37-.44,11.21s.15,7.52.43,11.12c2.8,2.15,5.78,4.25,8.9,6.29a2.56,2.56,0,0,1,.35.22c3.72,2.41,7.44,4.58,11.12,6.5,3.75-1.92,7.54-4.11,11.33-6.53,3.23-2.06,6.3-4.2,9.19-6.39.29-3.62.44-7.37.44-11.21s-.15-7.52-.43-11.12Zm-26.43-16a105.69,105.69,0,0,0-11.13-4.39,109.27,109.27,0,0,0-2.54,12.93Q36.79,41,39.6,39.22l.11-.07q4-2.58,8.11-4.82Zm-27.23,27c2.13,2.18,4.46,4.37,7,6.53q-.14-3.16-.13-6.41c0-2.22,0-4.4.14-6.56-2.5,2.13-4.83,4.29-7,6.44ZM59.78,88.55a105.69,105.69,0,0,0,11.13,4.39A109.27,109.27,0,0,0,73.45,80Q70.81,81.87,68,83.66c-2.74,1.75-5.49,3.38-8.22,4.89Z"/>
+                          
+                          </svg>
+                      </div>
+                      <h3 class="mt-0 mb-2 font-extrabold text-xl tracking-normal box-border"> Our Core Values   </h3>
+                      <p class="mt-0 mb-8 md:mb-0 box-border text-gray-200">Our Core Values revolve around Service, Integrity and Quality. </p>
+                  </div>
+               
+              </div>
+          </div>       
+      </section> 
+
+      <!-- Section 5 -->
+      <section class="relative leading-7 text-gray-900 bg-white border-solid pt-24 pb-24">
+        <div class="box-border mx-auto border-solid lg:pl-8 max-w-7xl">
+          <div class="flex flex-col items-center leading-7 text-gray-900 border-0 border-gray-200 lg:flex-row">
+            <div class="box-border flex flex-col justify-center w-full h-full p-8 text-gray-900 border-solid lg:w-1/2 md:p-16 lg:p-0 lg:pl-10 lg:pr-20">
+               <h1 class="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-4xl lg:text-4xl xl:text-4xl">Why Choose Us</h1>
+              <p class="mt-2 text-xl text-left border-0 border-gray-200 sm:text-2xl">We offer the best features in the industry.</p>
+              <div class="grid gap-4 mt-8 leading-7 border-0 border-gray-200 sm:mt-10 sm:gap-6 lg:mt-12 lg:gap-8">
+                <div class="box-border flex items-start text-gray-900 border-solid">
+                  <div class="flex items-center justify-center w-12 h-12 leading-7 bg-primary-600 border-0 border-gray-200 rounded-full">
+                    <p class="box-border m-0 text-xl text-white border-solid">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="white" class="bi bi-eye mx-auto items-baseline" viewBox="0 0 24 24">
+                          <path d="M22 17.002a6.002 6.002 0 0 1-4.713 5.86l-.638-1.914A4.003 4.003 0 0 0 19.465 19H17a2 2 0 0 1-2-2v-4a2 2 0 0 1 2-2h2.938a8.001 8.001 0 0 0-15.876 0H7a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2v-5C2 6.477 6.477 2 12 2s10 4.477 10 10V17.002z"/>
+                          
+                          </svg>
+                    </p>
+                  </div>
+                  <div class="flex-1 ml-6 leading-7 border-0 border-gray-200">
+                    <h3 class="box-border m-0 text-lg font-semibold leading-tight tracking-tight text-black border-solid sm:text-xl md:text-2xl">Service:</h3>
+                    <p class="box-border mt-2 text-base leading-normal text-gray-900 border-solid">We believe that customer service is an important key to success.  Therefore, we strive to ensure effective and smooth processes and procedures to our customers along with timely delivery. </p>
+                  </div>
+                </div>
+                <div class="box-border flex items-start text-gray-900 border-solid">
+                  <div class="flex items-center justify-center w-12 h-12 leading-7 bg-primary-600 border-0 border-gray-200 rounded-full">
+                    <p class="box-border m-0 text-xl text-white border-solid">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="white" class="bi bi-eye mx-auto items-baseline" viewBox="0 0 512 462.24">
+                          <path d="M211.02 0C132.6 49.71 61.75 73.23.95 67.67-9.67 282.45 69.65 409.3 210.21 462.24c15.15-5.53 29.6-11.96 43.31-19.32a36.277 36.277 0 0 1-12.56-8.15l-.06-.07-.06.07c-3-3-5.48-6.53-7.27-10.43-7.56 3.5-15.34 6.72-23.35 9.64C86.85 387.52 17.23 276.18 26.54 87.65c53.37 4.88 115.56-15.76 184.4-59.4 59.56 46.27 121.03 59.82 183.68 56.54.52 19.96.27 38.97-.7 57.09 8.27 1.21 16.35 4 24.15 8.57 2.25-26.82 3.03-55.47 2.22-86.04C348.92 68.15 278.88 52.71 211.02 0zm55.15 413.12a3.67 3.67 0 0 1-3.68-3.68c0-1.05.15-2.08.39-3.11 5.91-46.78 26.59-47.62 52.6-54.39 7.75-2.02 21.94-3.1 30.2-10.33 4.55-4 7.3-12.13 6.2-18-6.27-5.83-11.11-12.13-12.21-24.14l-.75.01c-1.75-.03-3.44-.41-5-1.31-3.47-1.99-5.37-5.74-6.29-10.07-1.16-5.45-.77-11.93-.2-16.01l.2-.81c1.21-3.37 2.71-5.2 4.62-5.99l.05-.03c-.87-16.23 1.87-41.94-14.79-47 32.91-40.68 70.87-62.82 99.37-26.62 31.75 1.67 45.91 48.47 26.19 73.65h-.83c1.89.79 3.39 2.62 4.6 5.99l.22.81c.57 4.08.95 10.56-.22 16.01-.92 4.33-2.81 8.08-6.28 10.07-1.56.9-3.25 1.28-4.99 1.31l-.76-.01c-1.09 12.01-5.93 18.31-12.2 24.14-1.12 5.87 1.65 14 6.19 18 8.27 7.23 22.45 8.3 30.22 10.33 26 6.77 46.68 7.61 52.59 54.39.24 1.03.39 2.06.39 3.11 0 2.04-1.65 3.68-3.68 3.68H266.17zM208.74 74.84c-53.04 33.61-100.95 49.51-142.06 45.76-7.18 145.23 46.45 231 141.5 266.8.6-.22 1.19-.45 1.79-.67V75.78l-1.23-.94z"/>
+                          
+                          </svg>
+                    </p>
+                  </div>
+                  <div class="flex-1 ml-6 leading-7 border-0 border-gray-200">
+                    <h3 class="box-border m-0 text-lg font-semibold leading-tight tracking-tight text-black border-solid sm:text-xl md:text-2xl">Integrity:</h3>
+                    <p class="box-border mt-2 text-base leading-normal text-gray-900 border-solid">We believe that integrity and respect of our customers and the environment is very important for our development and growth. </p>
+                  </div>
+                </div>
+                <div class="box-border flex items-start text-gray-900 border-solid">
+                  <div class="flex items-center justify-center w-12 h-12 leading-7 bg-primary-600 border-0 border-gray-200 rounded-full">
+                    <p class="box-border m-0 text-xl text-white border-solid">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="white" class="bi bi-eye mx-auto items-baseline" viewBox="0 0 92.35 122.88">
+                          <path d="M46.18,0a9.26,9.26,0,0,1,5.61,1.76C54,3.16,56.45,5.91,59.5,7.65c4.28,2.45,12.22-.93,16.29,5.11,2.37,3.52,2.48,6.28,2.66,9a15.84,15.84,0,0,0,3.72,9.64c5,6.59,6,11,3.45,15.56-1.75,3.11-5.44,4.85-6.29,6.82-1.82,4.21.19,7.37-2.3,12.27a13.05,13.05,0,0,1-7.93,6.78c-3,1-6-.43-8.39.58C56.5,75.19,53.39,79.3,50,80.34a13,13,0,0,1-7.73,0c-3.35-1-6.46-5.15-10.66-6.92-2.4-1-5.4.39-8.39-.58a13,13,0,0,1-7.94-6.78c-2.49-4.9-.48-8.06-2.3-12.27-.85-2-4.54-3.71-6.29-6.82-2.57-4.58-1.53-9,3.46-15.56a16,16,0,0,0,3.72-9.64c.17-2.73.28-5.49,2.66-9,4.06-6,12-2.66,16.29-5.11,3-1.74,5.51-4.49,7.7-5.88A9.29,9.29,0,0,1,46.18,0Zm-.55,25.87c.56-2.84,5.21-.23,5.51,4.34a19.52,19.52,0,0,1-.4,4.85h6.64c2.76.11,5.17,2.09,3.47,5.33.39,1.42.45,3.08-.61,3.73a5.45,5.45,0,0,1-1.64,4.71,5.58,5.58,0,0,1-.85,2.87c-.89,1.26-1.62,1-3,1H43.52a4.3,4.3,0,0,1-3.9-2V38.24c3.34-.9,5.11-5.48,6-8.48V25.88ZM31.76,36.47h5.08a1.06,1.06,0,0,1,1.06,1.06V51.65a1.06,1.06,0,0,1-1.06,1.07H31.76a1.07,1.07,0,0,1-1.06-1.07V37.53a1.07,1.07,0,0,1,1.06-1.06Zm2.92,9.37a1.34,1.34,0,1,1-1.34,1.34,1.34,1.34,0,0,1,1.34-1.34ZM3.39,113.07,15,111l5.73,10.25c4.15,5.15,6.79-3.31,8-6.26L39.78,94c-2.57-.89-5.66-3.47-8.85-6.35-6.35.13-12.27-1-16.62-6.51L1.5,105.85.38,108.23c-.87,3.09-.41,5.13,3,4.84Zm85.57,0L77.41,111l-5.73,10.25c-4.16,5.15-6.8-3.31-8-6.26L52.57,94c2.57-.89,5.67-3.47,8.85-6.35,6.35.13,12.27-1,16.62-6.51l12.82,24.75L92,108.23c.87,3.09.41,5.13-3,4.84ZM46.18,15.33A25.14,25.14,0,1,1,21,40.47,25.13,25.13,0,0,1,46.18,15.33Z"/>
+                          
+                          </svg>
+                    </p>
+                  </div>
+                  <div class="flex-1 ml-6 leading-7 border-0 border-gray-200">
+                    <h3 class="box-border m-0 text-lg font-semibold leading-tight tracking-tight text-black border-solid sm:text-xl md:text-2xl">Quality:</h3>
+                    <p class="box-border mt-2 text-base leading-normal text-gray-900 border-solid">We believe in procuring superior quality products for our customers and ensuring the maintenance of these standards. We believe that providing good quality products will create mutual trust between our company and our valued customers. </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="lg:w-2/5">
+              <img src="https://res.cloudinary.com/lamcom/image/upload/v1668697361/sravya/images/choose_jqqeez.jpg">
+            </div>
+          </div>
+        </div>
+      </section>  
+ 
+  
+</template>
+
+
---
+++ pages/about/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>    
+     <About />    
+  </div>
+</template>
+
+<script setup>
+definePageMeta({
+  //colorMode: 'system',
+  layout: "main",
+})
+const { data: color } = ref('white')
+const { data: services } = await useAsyncData('services-list', () => queryContent('services')
+  .only([
+    "title",
+    "description",
+    "coverimage",
+    "author",
+    "date",
+    "_path",
+    "tags",
+    "type",
+  ])
+  .find())
+</script>
