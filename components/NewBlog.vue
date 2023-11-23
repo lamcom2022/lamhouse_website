4571ff71d78153b53c80a6040058b9cf8d77ff40
R-Ravinther <61197918+R-Ravinther@users.noreply.github.com>
2023-11-23 10:15:38 +0530

    updated blog pages

--- components/NewBlog.vue
+++ components/NewBlog.vue
@@ -6,7 +6,7 @@
       class="capitalize font-semibold text-[38px] lg:text-7xl text-center leading-snug text-white"
     >
       <span class="font-extrabold bg-clip-text text-primary-500 uppercase"
-        >Team</span
+        >Blog</span
       >
     </h1>
 
@@ -45,92 +45,315 @@
     </div>
   </main>
 
-  <div class="bg-white pt-10 pb-20 px-4 sm:px-6 lg:pt-10 lg:pb-28 lg:px-8">
-    <div class="relative max-w-lg mx-auto lg:max-w-7xl">
-      <h2
-        class="text-3xl tracking-tight font-extrabold text-gray-600 sm:text-4xl"
-      >
-        Our Team
-      </h2>
-      <p class="text-base font-medium text-primary-900 mt-5">
-        We are an enthusiastic team of passionate designers, coders, animators
-        and developers, with years of knowledge and skill behind us. We work
-        hard every day to ensure your dreams and wishes turn into reality in
-        front of you, with your inputs and to your satisfaction. We are here to
-        meet and exceed your every expectation and requirement. <br />
-        After all – we believe that ‘every business must grow and be present
-        online – however small or large”!
-      </p>
-    </div>
-  </div>
-  <div class="max-w-7xl mx-auto">
-    <h2
-      class="text-3xl tracking-tight font-extrabold text-gray-600 sm:text-4xl"
-    >
-      Our Strength Include
-    </h2>
-    <div class="mt-10 max-w-7xl">
-      <dl class="space-y-10 md:space-y-0 md:grid md:gap-y-10">
-        <div v-for="team in team" :key="team.name" class="relative">
-          <dt>
-            <div
-              class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white"
-            >
-              <component :is="team.icon" class="h-6 w-6" aria-hidden="true" />
-            </div>
-            <p class="ml-16 text-lg leading-6 font-medium text-gray-900">
-              {{ team.name }}
-            </p>
-          </dt>
-        </div>
-      </dl>
-    </div>
-  </div>
+  <!-- Section 7 -->
+  <section class="w-full bg-white">
+    <div class="max-w-5xl px-10 py-10 mx-auto xl:px-0">
+      <div class="flex flex-wrap mt-10 overflow-hidden">
+        <div class="w-full overflow-hidden md:w-4/6 lg:w-4/6 xl:w-4/6">
+          <div class="ml-2 mr-2 md:mr-4">
+            <div class="grid grid-cols-12 gap-8">
+              <div class="col-span-12 md:col-span-6">
+                <a
+                  href="/blog"
+                  class="block h-48 mb-3 overflow-hidden rounded-lg"
+                >
+                  <img
+                    class="object-cover object-center w-full h-full transition duration-300 ease-out transform scale-100 hover:scale-105"
+                    src="https://res.cloudinary.com/lamcom/image/upload/v1682866786/lamhouse/blog/softsolution_ul0i4y.jpg"
+                    alt=""
+                  />
+                </a>
+                <h2 class="mb-2 font-serif text-lg text-gray-900">
+                  <a href="/blog"
+                    >Do small businesses need Software Solutions for their
+                    Businesses?</a
+                  >
+                </h2>
+                <p class="mb-3 text-sm text-gray-800">
+                  Any country gets 44% of its revenue from small businesses and
+                  their growth is attributed to the direct development of a
+                  country's economy...
+                  <a
+                    href="/blog"
+                    class="inline-flex items-center ml-1 text-xs text-gray-500 underline"
+                  >
+                    <span>Continue Reading</span>
+                    <svg
+                      class="w-3 h-3 ml-1 transform -rotate-45"
+                      fill="none"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M14 5l7 7m0 0l-7 7m7-7H3"
+                      ></path>
+                    </svg>
+                  </a>
+                </p>
+              </div>
 
-  <div
-    class="max-w-7xl mx-auto mt-16 my-16 lg:grid lg:grid-cols-3 md:flex md:flex-col py-10"
-  >
-    <div class="mx-auto px-2 lg:px-2 lg:py-10 md:py-10">
-      <img
-        class="w-full rounded"
-        src="https://res.cloudinary.com/lamcom/image/upload/v1694414436/lamhouse/muthur_xaksbh.jpg"
-      />
-      <div class="box-border h-32 w-full border-4 bg-primary-600 shadow-lg">
-        <p class="text-white text-center font-bold pt-10 text-2xl">
-          Muthuraman A
-        </p>
-        <p class="text-white text-center font-normal text-xl">CEO, Founder</p>
+              <div class="col-span-12 md:col-span-6">
+                <a
+                  href="/blog-1"
+                  class="block h-48 mb-3 overflow-hidden rounded-lg"
+                >
+                  <img
+                    class="object-cover object-center w-full h-full transition duration-300 ease-out transform scale-100 hover:scale-105"
+                    src="https://res.cloudinary.com/lamcom/image/upload/v1682866786/lamhouse/blog/digitalmarketing_ser5yo.jpg"
+                    alt=""
+                  />
+                </a>
+                <h2 class="mb-2 font-serif text-lg text-gray-900">
+                  <a href="/blog-1"
+                    >Why do Small Businesses Need Digital Marketing?</a
+                  >
+                </h2>
+                <p class="mb-3 text-sm text-gray-800">
+                  In this Digital world, we have seen many small businesses rise
+                  and many fall too...
+                  <a
+                    href="/blog-1"
+                    class="inline-flex items-center ml-1 text-xs text-gray-500 underline"
+                  >
+                    <span>Continue Reading</span>
+                    <svg
+                      class="w-3 h-3 ml-1 transform -rotate-45"
+                      fill="none"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M14 5l7 7m0 0l-7 7m7-7H3"
+                      ></path>
+                    </svg>
+                  </a>
+                </p>
+              </div>
+
+              <div class="col-span-12 md:col-span-6">
+                <a
+                  href="/blog-2"
+                  class="block h-48 mb-3 overflow-hidden rounded-lg"
+                >
+                  <img
+                    class="object-cover object-center w-full h-full transition duration-300 ease-out transform scale-100 hover:scale-105"
+                    src="https://res.cloudinary.com/lamcom/image/upload/v1683010721/lamhouse/blog/digital_xpdf9a.jpg"
+                    alt=""
+                  />
+                </a>
+                <h2 class="mb-2 font-serif text-lg text-gray-900">
+                  <a href="/blog-2">Does digital marketing work for you?</a>
+                </h2>
+                <p class="mb-3 text-sm text-gray-800">
+                  Most small business think that they do not need digital
+                  marketing, or do not know if they need it...
+                  <a
+                    href="/blog-2"
+                    class="inline-flex items-center ml-1 text-xs text-gray-500 underline"
+                  >
+                    <span>Continue Reading</span>
+                    <svg
+                      class="w-3 h-3 ml-1 transform -rotate-45"
+                      fill="none"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M14 5l7 7m0 0l-7 7m7-7H3"
+                      ></path>
+                    </svg>
+                  </a>
+                </p>
+              </div>
+
+              <div class="col-span-12 md:col-span-6">
+                <a
+                  href="/blog-3"
+                  class="block h-48 mb-3 overflow-hidden rounded-lg"
+                >
+                  <img
+                    class="object-cover object-center w-full h-full transition duration-300 ease-out transform scale-100 hover:scale-105"
+                    src="https://res.cloudinary.com/lamcom/image/upload/v1683265020/lamhouse/blog/namaste_oaddfu.jpg"
+                    alt=""
+                  />
+                </a>
+                <h2 class="mb-2 font-serif text-lg text-gray-900">
+                  <a href="/blog-3"
+                    >5 Things COVID – 19 making us go back to our traditional
+                    ways of living.</a
+                  >
+                </h2>
+                <p class="mb-3 text-sm text-gray-800">
+                  It might be a weird but an apt question in the current
+                  scenario. You just need ...
+                  <a
+                    href="/blog-3"
+                    class="inline-flex items-center ml-1 text-xs text-gray-500 underline"
+                  >
+                    <span>Continue Reading</span>
+                    <svg
+                      class="w-3 h-3 ml-1 transform -rotate-45"
+                      fill="none"
+                      stroke="currentColor"
+                      viewBox="0 0 24 24"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M14 5l7 7m0 0l-7 7m7-7H3"
+                      ></path>
+                    </svg>
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Begining of Sidebar -->
+        <div
+          class="w-full mt-12 overflow-hidden md:w-2/6 lg:w-2/6 xl:w-2/6 md:mt-0"
+        >
+          <div class="ml-2 mr-2 md:ml-4">
+            <div class="mt-0">
+              <h2 class="mb-5 text-lg font-medium text-gray-900">Categories</h2>
+              <ul>
+                <li class="flex">
+                  <a
+                    href="blog"
+                    class="flex-1 block py-2 font-serif text-lg font-thin text-gray-900"
+                    >Software</a
+                  ><span class="p-2 text-lg font-thin text-gray-700">1</span>
+                </li>
+                <li class="flex">
+                  <a
+                    href="blog-2"
+                    class="flex-1 block py-2 font-serif text-lg font-thin text-gray-900"
+                    >Digital Marketing</a
+                  ><span class="p-2 text-lg font-thin text-gray-700">2</span>
+                </li>
+                <li class="flex">
+                  <a
+                    href="/blog-3"
+                    class="flex-1 block py-2 font-serif text-lg font-thin text-gray-900"
+                    >General</a
+                  ><span class="p-2 text-lg font-thin text-gray-700">1</span>
+                </li>
+              </ul>
+            </div>
+
+            <div class="mt-16">
+              <h2 class="mb-5 text-lg font-medium text-gray-900">
+                Recent Posts
+              </h2>
+              <ul>
+                <li class="mb-5">
+                  <a href="/blog" class="flex">
+                    <div class="w-1/3 overflow-hidden rounded">
+                      <img
+                        class="object-cover object-center w-full h-full transition duration-300 ease-out transform scale-100 rounded hover:scale-105"
+                        src="https://res.cloudinary.com/lamcom/image/upload/v1682866786/lamhouse/blog/softsolution_ul0i4y.jpg"
+                        alt=""
+                      />
+                    </div>
+                    <div
+                      class="flex flex-col items-start justify-center w-2/3 p-2"
+                    >
+                      <h3 class="mb-2 font-serif font-thin text-gray-900">
+                        Do small businesses need Software Solutions for their
+                        Businesses?
+                      </h3>
+                      <span class="block text-xs font-thin text-gray-800"
+                        >January 02, 2023</span
+                      >
+                    </div>
+                  </a>
+                </li>
+                <li class="mb-5">
+                  <a href="/blog-1" class="flex">
+                    <div class="w-1/3 overflow-hidden rounded">
+                      <img
+                        class="object-cover object-center w-full h-full transition duration-300 ease-out transform scale-100 rounded hover:scale-105"
+                        src="https://res.cloudinary.com/lamcom/image/upload/v1682866786/lamhouse/blog/digitalmarketing_ser5yo.jpg"
+                        alt=""
+                      />
+                    </div>
+                    <div
+                      class="flex flex-col items-start justify-center w-2/3 p-2"
+                    >
+                      <h3 class="mb-2 font-serif font-thin text-gray-900">
+                        Why do Small Businesses Need Digital Marketing?
+                      </h3>
+                      <span class="block text-xs font-thin text-gray-800"
+                        >January 04, 2023</span
+                      >
+                    </div>
+                  </a>
+                </li>
+                <li class="mb-5">
+                  <a href="/blog-2" class="flex">
+                    <div class="w-1/3 overflow-hidden rounded">
+                      <img
+                        class="object-cover object-center w-full h-full transition duration-300 ease-out transform scale-100 rounded hover:scale-105"
+                        src="https://res.cloudinary.com/lamcom/image/upload/v1683010721/lamhouse/blog/digital_xpdf9a.jpg"
+                        alt=""
+                      />
+                    </div>
+                    <div
+                      class="flex flex-col items-start justify-center w-2/3 p-2"
+                    >
+                      <h3 class="mb-2 font-serif font-thin text-gray-900">
+                        Does digital marketing work for you?
+                      </h3>
+                      <span class="block text-xs font-thin text-gray-800"
+                        >May 02, 2023</span
+                      >
+                    </div>
+                  </a>
+                </li>
+                <li class="mb-5">
+                  <a href="/blog-3" class="flex">
+                    <div class="w-1/3 overflow-hidden rounded">
+                      <img
+                        class="object-cover object-center w-full h-full transition duration-300 ease-out transform scale-100 rounded hover:scale-105"
+                        src="https://res.cloudinary.com/lamcom/image/upload/v1683265020/lamhouse/blog/namaste_oaddfu.jpg"
+                        alt=""
+                      />
+                    </div>
+                    <div
+                      class="flex flex-col items-start justify-center w-2/3 p-2"
+                    >
+                      <h3 class="mb-2 font-serif font-thin text-gray-900">
+                        5 Things COVID – 19 making us go back to our traditional
+                        ways of living.
+                      </h3>
+                      <span class="block text-xs font-thin text-gray-800"
+                        >April 27, 2020</span
+                      >
+                    </div>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-    <div class="mx-auto px-2 lg:px-2 lg:py-10 md:py-10">
-      <img
-        class="w-full rounded-t"
-        src="https://res.cloudinary.com/lamcom/image/upload/v1662786421/lamhouse/ravi_ynbww9.jpg"
-      />
-      <div class="box-border h-32 w-full border-4 bg-primary-600 shadow-lg">
-        <p class="text-white text-center font-bold pt-10 text-2xl">
-          Ravinther R
-        </p>
-        <p class="text-white text-center font-normal text-xl">
-          Design, Developer
-        </p>
-      </div>
-    </div>
-    <div class="mx-auto px-2 lg:px-2 lg:py-10 md:py-10">
-      <img
-        class="w-full rounded"
-        src="https://res.cloudinary.com/lamcom/image/upload/v1662786765/lamhouse/sripathy_kxethl.jpg"
-      />
-      <div class="box-border h-32 w-full border-4 bg-primary-600 shadow-lg">
-        <p class="text-white text-center font-bold pt-10 text-2xl">
-          Sreepathy R
-        </p>
-        <p class="text-white text-center font-normal text-xl">
-          Head, Digital Marketing
-        </p>
-      </div>
-    </div>
-  </div>
+  </section>
 </template>
 
 <script>
